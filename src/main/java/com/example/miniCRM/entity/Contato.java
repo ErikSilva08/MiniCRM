dca6e6e812563771374da08a496c36df19fbd3d1
ErikSilva08 <eriksilvaf89@gmail.com>
2025-09-29 15:35:10 -0300

    Creation of the contato class with entity relationship

---
+++ src/main/java/com/example/miniCRM/entity/Contato.java
@@ -0,0 +1,29 @@
+package com.example.miniCRM.entity;
+
+import com.example.miniCRM.enumeration.TipoContato;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_contatos")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private TipoContato type;
+    @Column(length = 100, nullable = false,unique = true)
+    private String valueType;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+
+}
