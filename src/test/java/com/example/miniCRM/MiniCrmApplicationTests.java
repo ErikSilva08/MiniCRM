89516c9b513dd46d827db92ebd76dabe48cec6d4
ErikSilva08 <eriksilvaf89@gmail.com>
2025-09-29 15:11:12 -0300

    first commit

---
+++ src/test/java/com/example/miniCRM/MiniCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
